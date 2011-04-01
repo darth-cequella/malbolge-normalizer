67024744a368e784d72473eef1a038b0b1ad1d0e
darth-cequella <cequelldev@gmail.com>
2011-04-01 02:26:42 -0300

    Add de-normalize/expand option

--- malbolge-normalizer.go
+++ malbolge-normalizer.go
@@ -48,7 +48,7 @@ func normalize(malbolgeFile string) {
 	}
 	
 	//------------------------------------------------------------------
-	//Content readed let's preset content to normalized version
+	//Content readed, let's preset content to normalized version
 	for i,b := range content {
 		content[i] = byte( (int(b)+i)%94 )
 		
@@ -87,7 +87,7 @@ func numberNormalize(malbolgeFile string) {
 	}
 	
 	//------------------------------------------------------------------
-	//Content readed let's preset content to normalized version
+	//Content readed, let's preset content to normalized version
 	var numberContent []int
 	for i,b := range content {
 		aux := int( int(b)+i)%94
@@ -96,8 +96,68 @@ func numberNormalize(malbolgeFile string) {
 	
 	fmt.Println(numberContent)
 }
+func expand(malbolgeFile string) {
+	file, err := os.Open(malbolgeFile) //Open malbolge file
+	
+	if err != nil {
+		fmt.Println("File `"+malbolgeFile+"` not Found")
+		log.Fatalln(err)
+	}
+	
+	reader := bufio.NewReader(file) //Create a reader
+	
+	var content []byte //Catch the content of the file
+	
+	line,_,err := reader.ReadLine()
+	for line != nil {
+		if err != nil {
+			fmt.Println("Error reading `"+malbolgeFile+"` file")
+			log.Fatal(err)
+			break
+		}
+		
+		for _, b := range line { //Concat content with new byte array
+			content = append(content, b)
+		}
+		line,_,_ = reader.ReadLine() //Read next line
+	}
+	
+	//------------------------------------------------------------------
+	//Content readed, let's extend content from normalized version
+	opContent := ""
+	for i,b := range content {
+		var aux byte
 
-func extend() {
+		switch(b){
+			case 'i':
+				aux = byte(4)
+			case '<':
+				aux = byte(5)
+			case '/':
+				aux = byte(23)
+			case '*':
+				aux = byte(29)
+			case 'j':
+				aux = byte(40)
+			case 'p':
+				aux = byte(62)
+			case 'o':
+				aux = byte(68)
+			case 'v':
+				aux = byte(81)
+		}
+
+		aux -= byte(i)
+		if int(aux) < 33 {
+			aux += byte(94)
+		} else if int(aux) > 126 {
+			 aux -= byte(94)
+		}
+
+		opContent += string(aux)
+	}
+	
+	fmt.Println(opContent)
 }
 
 func main() {
@@ -108,5 +168,8 @@ func main() {
 			numberNormalize(args[1])
 		case "-n":
 			normalize(args[1])
+		case "-e":
+			expand(args[1])
 	}
 }
+ 
